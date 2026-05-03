230109b27bd33d0c559fc273c65be1617d1bc0fd
Test <test@example.com>
2026-05-03 19:23:48 +0530

    ios

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -24,13 +24,46 @@ kotlin {
     }
     jvm()
 
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "common"
+            isStatic = true
+        }
+    }
+
     sourceSets {
         commonMain {
             dependencies {
                 api(libs.bundles.kotlinx)
+            }
+        }
+        
+        val androidMain by getting {
+            dependencies {
                 api(libs.okhttp)
                 api(libs.protobuf.java)
             }
+        }
+        
+        val jvmMain by getting {
+            dependencies {
+                api(libs.okhttp)
+                api(libs.protobuf.java)
+            }
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain.get())
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
     }
 }
